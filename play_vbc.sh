eb316646062f8a0ef3971054be5ee319372c7fa8
Marco-Yang <3155853275@qq.com>
2026-05-07 17:15:59 +0800

    feat : initialize bcv with fpg reward

---
+++ play_vbc.sh
@@ -0,0 +1,4 @@
+python scripts/reinforcement_learning/rsl_rl/play.py \
+ --task=RobotLab-Isaac-WBC-Flat-Unitree-Go2-X5-Play-v0 \
+ --checkpoint=logs/rsl_rl/unitree_go2_x5_velocity_pose_flat/2026-05-05_15-38-27_export/model_41200.pt \
+ --num_envs 1 \
---
+++ train_vbc.sh
@@ -0,0 +1,4 @@
+python scripts/reinforcement_learning/rsl_rl/train.py \
+ --task=RobotLab-Isaac-WBC-Flat-Unitree-Go2-X5-v0 \
+ --num_envs 1 \
+ --headless
